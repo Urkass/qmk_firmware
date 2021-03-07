89c3c6830e3405592c3d72ed6bcdc065ed324372
pashutk <me@pashutk.ru>
2021-03-07 22:34:26 +0300

    Add brightnes controls

--- keyboards/handwired/void40/keymaps/pashutk/keymap.c
+++ keyboards/handwired/void40/keymaps/pashutk/keymap.c
@@ -106,14 +106,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      | Left | Down | Right|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | LCtrl| LAlt | LGUI |      |      |      |      |      |      |      |      |
+ * |      | LCtrl| LAlt | LGUI |      |      |      |      |      |  Br- |  Br+ |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_FUNCTION] = LAYOUT_planck_mit(
     KC_NO,   KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
     KC_NO,   KC_F11,    KC_F12,  KC_NO,   KC_NO,   KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,
     KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,
-    KC_TRNS, KC_LCTL,   KC_LALT, KC_LGUI, KC_TRNS,      KC_NO,   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO
+    KC_TRNS, KC_LCTL,   KC_LALT, KC_LGUI, KC_TRNS,      KC_NO,   KC_TRNS, KC_NO,   KC_BRID, KC_BRIU, KC_NO
 )
 };
 
